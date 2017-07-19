2d142dc4ab6020c6103e51884c62fe01ab53e2cb
Haiping <haiping008@gmail.com>
2017-07-18 21:26:06 -0500

    refactor

---
+++ Leopard/Leopard/Core/System/DbInitializer.cs
@@ -0,0 +1,23 @@
+﻿using Leopard.DataContexts;
+using Leopard.DbTables;
+using Leopard.Enums;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Leopard.Core
+{
+    public class DbInitializer
+    {
+        public static void Initialize(IHostingEnvironment env)
+        {
+            CoreDbContext context = new CoreDbContext(new DbContextOptions<CoreDbContext>());
+            //var dbContexts = serviceProvider.GetService<DataContexts>();
+
+            context.Database.EnsureCreated();
+        }
+    }
+}
