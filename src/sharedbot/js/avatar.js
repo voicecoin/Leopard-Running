457a7ece546545cb105c924c0699d7646dc007a1
Yuancheng Zhang <old.far.hot@gmail.com>
2018-06-29 22:14:16 +0800

    更新预设头像路径

--- src/sharedbot/js/avatar.js
+++ src/sharedbot/js/avatar.js
@@ -2,23 +2,23 @@
 
 var defaultBotAvatars = {
     default: [
-        "/src/voicecoin/img/avatar/default0.png",
-        "/src/voicecoin/img/avatar/default1.png",
+        "/src/sharedbot/img/avatar/default0.png",
+        "/src/sharedbot/img/avatar/default1.png",
     ],
     Voiceweb: [
-        "/src/voicecoin/img/avatar/voicebot0.png",
-        "/src/voicecoin/img/avatar/voicebot0.png",
+        "/src/sharedbot/img/avatar/voicebot0.png",
+        "/src/sharedbot/img/avatar/voicebot0.png",
     ],
     AppleStore: [
-        "/src/voicecoin/img/avatar/apple0.png",
-        "/src/voicecoin/img/avatar/apple0.png",
+        "/src/sharedbot/img/avatar/apple0.png",
+        "/src/sharedbot/img/avatar/apple0.png",
     ],
     Yayabot: [
-        "/src/voicecoin/img/avatar/yaya0.png",
-        "/src/voicecoin/img/avatar/yaya1.png",
+        "/src/sharedbot/img/avatar/yaya0.png",
+        "/src/sharedbot/img/avatar/yaya1.png",
     ],
     Spotify: [
-        "/src/voicecoin/img/avatar/spotify0.png",
-        "/src/voicecoin/img/avatar/spotify0.png",
+        "/src/sharedbot/img/avatar/spotify0.png",
+        "/src/sharedbot/img/avatar/spotify0.png",
     ],
 };
