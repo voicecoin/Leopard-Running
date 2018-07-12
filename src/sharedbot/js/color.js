cba11d1c7293f3987338bbff629400e76176fe65
Yuancheng Zhang <old.far.hot@gmail.com>
2018-07-12 20:25:49 +0800

    恢复bot背景 #1

--- src/sharedbot/js/color.js
+++ src/sharedbot/js/color.js
@@ -53,9 +53,9 @@ function updateColor() {
   color2 = "rgb(" + r2 + "," + g2 + "," + b2 + ")";
 
 
-  //  $('#canvas-bg').css({
-  //    background: "-webkit-gradient(linear, left top, right top, from("+color1+"), to("+color2+"))"}).css({
-  //     background: "-moz-linear-gradient(left, "+color1+" 0%, "+color2+" 100%)"});
+   $('#canvas-bg').css({
+     background: "-webkit-gradient(linear, left top, right top, from("+color1+"), to("+color2+"))"}).css({
+      background: "-moz-linear-gradient(left, "+color1+" 0%, "+color2+" 100%)"});
 
   step += gradientSpeed;
   if (step >= 1) {
